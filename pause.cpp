56d4a092b33320b2399b90df9096dc1dbad1b93f
Ron Parker <ron@parkrrrr.com>
2017-02-02 16:41:15 +0000

    add a bunch of other simple message-sending utilities

---
+++ pause.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <boost/interprocess/ipc/message_queue.hpp>
+
+#include "structs.h"
+
+// main
+int main( int argc, char **argv )
+{
+    boost::interprocess::message_queue messageQueue( boost::interprocess::open_only, "thermo_message_queue");
+    
+    Message message;
+    
+    message.messageID = Message::PAUSE;
+    
+    messageQueue.send( &message, sizeof(Message), 0);
+    
+    return 0;
+    }
