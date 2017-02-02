28e824e4be5fc5e451a0bf852ac45d5a8d999c3a
Ron Parker <ron@parkrrrr.com>
2017-02-02 15:01:17 +0000

    general cleanup

--- structs.h
+++ structs.h
@@ -1,36 +1,36 @@
 struct Message {
-	static const int QUIT = 1;
-	static const int CANCEL = 2;
-	static const int SET = 3;
-	static const int START = 4;
-	static const int PAUSE = 5;
-	static const int RESUME = 6;
-	
-	int messageID;
-	int param1;
-	int param2;
+    static const int QUIT = 1;
+    static const int CANCEL = 2;
+    static const int SET = 3;
+    static const int START = 4;
+    static const int PAUSE = 5;
+    static const int RESUME = 6;
+    
+    int messageID;
+    int param1;
+    int param2;
 };
 
 struct Shared {
-	int sv;	
-	int pv;
-	int segTimeElapsed;
-	int progTimeElapsed;
-	int firingID;
-	int stepID;	
+    int sv;    
+    int pv;
+    int segTimeElapsed;
+    int progTimeElapsed;
+    int firingID;
+    int stepID;    
 };
 
 class SegmentType {
 public:
-	static const int AFAP = 1;
-	static const int Hold = 2;
-	static const int Pause = 3;
-	static const int Ramp = 4;
-	
-	int value; 
-	
-	SegmentType(void) {value = Pause;}
-	SegmentType &operator =(int newValue ) {value = newValue;return *this;}
-	operator int() {return value;}
+    static const int AFAP = 1;
+    static const int Hold = 2;
+    static const int Pause = 3;
+    static const int Ramp = 4;
+    
+    int value; 
+    
+    SegmentType(void) {value = Pause;}
+    SegmentType &operator =(int newValue ) {value = newValue;return *this;}
+    operator int() {return value;}
 };
 
