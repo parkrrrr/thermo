276fc4550ea7271b6f7e528ad18127c906c81d71
Ron Parker <ron@parkrrrr.com>
2017-02-02 20:41:33 +0000

    Add database functionality to server

--- server.cpp
+++ server.cpp
@@ -8,6 +8,7 @@
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/interprocess/shared_memory_object.hpp>
 #include <boost/interprocess/ipc/message_queue.hpp>
+#include <sqlite3.h>
 #include <SQLiteCpp/SQLiteCpp.h>
 
 #include "structs.h"
@@ -15,7 +16,7 @@
 static const std::string device = "/dev/ttyUSB0";
 static const int pv_margin = 5; // degrees Fahrenheit either side of SV
 
-static const char *commandNames[] = {"     "," AFAP"," Hold","Pause"," Ramp"};
+static const char *commandNames[] = {"     ","AFAP ","Hold ","Pause","Ramp "};
 
 class Segment {
 public:
@@ -42,7 +43,9 @@ public:
     boost::interprocess::mapped_region *mappedRegion;
     boost::interprocess::message_queue *messageQueue;
     Shared *shared;
-    
+   
+    SQLite::Database database;
+ 
     bool abort;
     
     Segment segment;
@@ -82,6 +85,9 @@ public:
     // next segment
     void NextSegment(void) {
         WriteFiringRecord();
+        shared->progTimeElapsed += shared->segTimeElapsed;
+        shared->segTimeElapsed = 0;
+        
         ResetSegmentStart();
         if ( !segmentQueue.empty()) {
             segment = segmentQueue.front();
@@ -92,6 +98,12 @@ public:
         }
         if ( segment.type == SegmentType::AFAP ) {
             SetSV(segment.targetSV, false );
+        }
+        if ( segment.type == SegmentType::Pause && segment.targetSV ) {
+            SetSV(segment.targetSV, false );
+        }
+        if (shared->firingID) {
+            ++shared->stepID;
         } 
     }
     
@@ -193,12 +205,13 @@ public:
                 if (reg == 1 ) {
                     // store in shared mem
                     shared->pv = temperature;
+                    shared->segTimeElapsed = ElapsedTime();
                     std::cout << "  " << 
                         commandNames[segment.type] << " | " <<
                         "PV " << shared->pv << " | " << 
                         "SV " << shared->sv << " / " << 
                                  segment.targetSV << " | " << 
-                        "Time " << boost::posix_time::to_simple_string(boost::posix_time::second_clock::universal_time()-segmentStartTime)
+                        "Time " << boost::posix_time::to_simple_string(boost::posix_time::seconds(shared->segTimeElapsed))
                                 << " / " <<
                                    boost::posix_time::to_simple_string(boost::posix_time::seconds(segment.rampTime)) << 
                         "         \r";
@@ -219,7 +232,16 @@ public:
     }
 
     void WriteFiringRecord( void ) {
-        // TODO:
+        if ( shared->firingID && shared->stepID ) {
+            SQLite::Statement statement(database,
+               "INSERT INTO \"Firings\" VALUES (?,?,?,?,?)");
+            statement.bind(1, shared->firingID);
+            statement.bind(2, shared->stepID );
+            statement.bind(3, commandNames[segment.type] );
+            statement.bind(4, shared->pv );
+            statement.bind(5, shared->segTimeElapsed); 
+            statement.exec();
+        } 
     }
     
     // cancel msg handler
@@ -230,9 +252,10 @@ public:
         // erase remainder of program
         segmentQueue.clear();
         
-        // TODO:
         // reset firing information
-        
+        shared->firingID = shared->stepID = 0;
+        shared->segTimeElapsed = shared->progTimeElapsed = 0;
+ 
         // set SV to 0 degrees
         std::stringstream stream;
         SetSV( 0, false );
@@ -243,14 +266,64 @@ public:
         segment.rampTime = 0;
     }
 
+    // create a firing record for this firing
+    void CreateFiring(int programID) {
+        boost::posix_time::ptime now = boost::posix_time::second_clock::universal_time();
+        boost::posix_time::time_facet *facet = new boost::posix_time::time_facet("%Y-%m-%d %H:%M:%S%F%Q");
+        std::stringstream stream;
+        stream.imbue(std::locale(stream.getloc(), facet));
+        stream << now;
+ 
+        SQLite::Statement statement(database, 
+            "INSERT INTO FiringInfo VALUES( NULL, ?, ?, NULL)");
+        statement.bind(1, programID > 0 ? programID : NULL );
+        statement.bind(2, stream.str());
+        statement.exec();
+        shared->firingID = database.getLastInsertRowid(); 
+        shared->stepID = 0;
+    } 
+
     // start msg handler
     void StartMessageHandler( int programID, int segmentID ) {
         CancelMessageHandler();
+       
+        SQLite::Statement query( database, "SELECT instruction, temperature, param FROM Programs WHERE programID = ? AND step >= ?");
+        query.bind(1, programID); 
+        query.bind(2, segmentID); 
+
+        int previousTemp = shared->pv;
         
-        // TODO:
-        // load program
-        // skip to specified segment
-        // If current start segment is ramp, modify timeout as needed
+        while ( query.executeStep()) {
+            const char *instruction = query.getColumn(0);
+            int temp = query.getColumn(1);
+            int param = query.getColumn(2);
+
+            Segment newseg;
+            newseg.targetSV = temp;
+            newseg.rampTime = 0;
+
+            switch (tolower(instruction[0])) { 
+            case 'h': // hold - param is seconds to hold
+                newseg.type = SegmentType::Hold;
+                newseg.rampTime = param;
+                break;
+            case 'a': // afap
+                newseg.type = SegmentType::AFAP;
+                break;
+            case 'p': // pause
+                newseg.type = SegmentType::Pause;
+                break;
+            case 'r': // ramp - param is degrees/hour
+                newseg.type = SegmentType::Ramp;
+                newseg.rampTime = 3600 * labs(temp-previousTemp) / param;
+            default:
+                break; 
+            }
+            segmentQueue.push_back(segment);
+            previousTemp = temp;
+        }
+        CreateFiring(programID);
+        NextSegment(); 
     }
 
     // pause msg handler
@@ -259,23 +332,26 @@ public:
         if ( segment.type == SegmentType::Pause ) {
             return;
         }
-        
+
+        Segment newseg(segment);
+ 
         // insert remainder of current segment after current segment
-        if ( segment.type == SegmentType::Ramp ) {
+        if ( newseg.type == SegmentType::Ramp ) {
             // adjust ramp time by comparing SV to target SV and scaling
-            segment.rampTime = segment.rampTime * (shared->sv - segmentStartTemp) / (segment.targetSV-segmentStartTemp);
+            newseg.rampTime = newseg.rampTime * (shared->sv - segmentStartTemp) / (newseg.targetSV-segmentStartTemp);
         }
-        if ( segment.type == SegmentType::Hold) {
+        if ( newseg.type == SegmentType::Hold) {
             // adjust hold time by subtracting time held so far
-            segment.rampTime -= ElapsedTime();
+            newseg.rampTime -= ElapsedTime();
         }
-        segmentQueue.push_front(segment);
+        segmentQueue.push_front(newseg);
 
         // change current segment to pause at current PV
-        segment.type = SegmentType::Pause;
-        segment.targetSV = shared->pv;
-        segment.rampTime = 0;
-        SetSV( segment.targetSV, false );
+        newseg.type = SegmentType::Pause;
+        newseg.targetSV = shared->pv;
+        newseg.rampTime = 0;
+        segmentQueue.push_front(newseg);
+        NextSegment();
     } 
 
     // resume msg handler
@@ -306,6 +382,7 @@ public:
         segmentQueue.push_back(newseg);
 
         // run new program
+        CreateFiring(-1);
         NextSegment();
     }
 
@@ -314,7 +391,9 @@ public:
     ioService(),
     serialPort(ioService, device),
     readBuffer(),
-    timer(ioService)
+    timer(ioService),
+    database("thermo.db",SQLITE_OPEN_READWRITE)
+
     {
         // create shared mem
         boost::interprocess::shared_memory_object::remove( "thermo_shared_memory");
