bf55ec05bfff020a57f568a20457ed4f70d8ff3f
Ron Parker <ron@parkrrrr.com>
2017-02-02 03:41:27 +0000

    moved shared structures to .h file

--- server.cpp
+++ server.cpp
@@ -8,44 +8,10 @@
 #include <boost/interprocess/shared_memory_object.hpp>
 #include <boost/interprocess/ipc/message_queue.hpp>
 
+#include "structs.h"
+
 static const std::string device = "/dev/ttyUSB0";
 static const int pv_margin = 5; // degrees Fahrenheit either side of SV
-
-struct Message {
-	static const int QUIT = 1;
-	static const int CANCEL = 2;
-	static const int SET = 3;
-	static const int START = 4;
-	static const int PAUSE = 5;
-	static const int RESUME = 6;
-	
-	int messageID;
-	int param1;
-	int param2;
-};
-
-struct Shared {
-	int sv;	
-	int pv;
-	int segTimeElapsed;
-	int progTimeElapsed;
-	int firingID;
-	int stepID;	
-};
-
-class SegmentType {
-public:
-	static const int AFAP = 1;
-	static const int Hold = 2;
-	static const int Pause = 3;
-	static const int Ramp = 4;
-	
-	int value; 
-	
-	SegmentType(void) {value = Pause;}
-	SegmentType &operator =(int newValue ) {value = newValue;return *this;}
-	operator int() {return value;}
-};
 
 class Direction {
 public:
